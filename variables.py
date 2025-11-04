7070c44bc04c56f8371e514a789f0469193be263
SumitKumar Padhy <sumitpadhy2006@gmail.com>
2025-11-05 01:22:52 +0530

    Create variables.py with variable types and examples
    Added variable types and examples in Python.

---
+++ variables.py
@@ -0,0 +1,18 @@
+#variable types in python
+
+name = "sumit" #string type
+age = 19       #integer type
+
+#comments can be also written like this
+"multi line comments can be"
+" written like this"
+
+price = 16.66  #float type
+old = True     #boolean type
+a = None        #none type
+
+print( " My name is : " , name)
+print((age))
+print((price))
+print(type(old))
+print(type(a))
